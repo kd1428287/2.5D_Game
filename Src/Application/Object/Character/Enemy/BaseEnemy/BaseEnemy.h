3e8d95f1eb4d33a9948762883f2f7d0b54736f1c
kd1428287 <kd1428287@st.kobedenshi.ac.jp>
2026-05-25 14:59:17 +0900

    second
    Player外殻形成中

---
+++ Src/Application/Object/Character/Enemy/BaseEnemy/BaseEnemy.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+class BaseEnemy : public KdGameObject
+{
+public:
+	BaseEnemy() {};
+	~BaseEnemy()override {};
+
+	void Init()override;
+	void Update(float dt)override;
+	void GenerateDepthMapFromLight()override;
+	void DrawLit()override;
+};
---
+++ Src/Application/Object/Character/Player/Player.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+enum class PlayerState
+{
+	Idle,
+	Walk_start,
+	Walk_middle,
+	Walk_end,
+	Attack_start,
+	Attack_middle,
+	Attack_end,
+};
+
+class Player : public KdGameObject
+{
+public:
+	Player() {};
+	~Player()override {};
+
+	void Init()override;
+	void Update(float dt)override;
+	
+	void GenerateDepthMapFromLight()override;
+	void DrawLit()override;
+
+private:
+	void ActiveInput();
+
+private:
+	std::shared_ptr<KdSquarePolygon> m_model = nullptr;
+};
